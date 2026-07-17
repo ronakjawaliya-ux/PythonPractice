ed94be8844edef048122b2e7935e2e7034950547
Ronak Jawalia <ronakjawaliya@gmail.com>
2026-07-17 14:49:10 +0530

    starting the map function

---
+++ 08_Functions/map_function.py
@@ -0,0 +1,8 @@
+# map() = applies a function to each item in an iterable (list, tuple, etc.)
+#
+# map(function,iterable)
+
+store = [("shirt",20.00),
+         ("pants",25.00),
+         ("jacket",50.00),
+         ("socks",10.00)]
