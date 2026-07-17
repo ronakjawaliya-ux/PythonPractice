9286230b94b03cbcf77a046dcb1ebe7d081307bc
Ronak Jawalia <ronakjawaliya@gmail.com>
2026-07-17 17:19:47 +0530

    Learned and practiced Map func. in Python

--- 08_Functions/map_function.py
+++ 08_Functions/map_function.py
@@ -6,3 +6,10 @@ store = [("shirt",20.00),
          ("pants",25.00),
          ("jacket",50.00),
          ("socks",10.00)]
+
+to_dollars = lambda data: (data[0],data[1]*96.22)    # (1 USD = 96.22 INR)
+
+store_dollars = list(map(to_dollars, store))
+
+for i in store_dollars:
+    print(i)
