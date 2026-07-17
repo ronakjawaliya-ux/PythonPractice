a841958a7dd85a54865392a5fdb23fc5de88e85e
Ronak Jawalia <ronakjawaliya@gmail.com>
2026-07-17 18:04:48 +0530

    Different functionsin python

--- 08_Functions/map_function.py
+++ 08_Functions/map_function.py
@@ -13,3 +13,19 @@ store_dollars = list(map(to_dollars, store))
 
 for i in store_dollars:
     print(i)
+
+
+
+
+store2 = [("shirt",2400.00),
+         ("pants",2700.00),
+         ("jacket",4500.00),
+         ("socks",300.00)]
+
+to_dollars = lambda data: (data[0],data[1]*96.22)  #INR --> USD  (1 USD = 96.22 INR)
+to_euros = lambda data: (data[0],data[1]*110.38)     #INR --> EUR  (1 EUR = 110.38 INR)
+
+store_euros = list(map(to_euros, store))
+
+for i in store_euros:
+    print(i)
